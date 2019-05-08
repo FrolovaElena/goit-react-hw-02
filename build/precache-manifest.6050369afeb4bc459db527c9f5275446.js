f1569e9cba5002bc5f4a80d28d8178aff3dc7c1f
Elena Frolova <elenschip777@gmail.com>
2019-05-08 14:31:16 +0300

    hw-02

---
+++ build/precache-manifest.6050369afeb4bc459db527c9f5275446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bd355fcfca83d8dad3b9f9383f0a31",
+    "url": "/goit-react-hw-02/index.html"
+  },
+  {
+    "revision": "3971e56508791794b318",
+    "url": "/goit-react-hw-02/static/css/main.7ffe28fd.chunk.css"
+  },
+  {
+    "revision": "f5e3d2b1e078d8513277",
+    "url": "/goit-react-hw-02/static/js/2.7be90313.chunk.js"
+  },
+  {
+    "revision": "3971e56508791794b318",
+    "url": "/goit-react-hw-02/static/js/main.9e54445b.chunk.js"
+  },
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  }
+]);
